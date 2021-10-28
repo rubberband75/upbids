4f82daa15cb089ac36272f9e0b89eac6c15217b0
Chandler Childs <chandler@advenna.com>
2021-10-27 22:47:09 -0600

    initial commit for upbids

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,9 @@ import Layout from '../components/layout'
 export default function Page () {
   return (
     <Layout>
-      <h1>NextAuth.js Example</h1>
+      <h1>UpBids</h1>
       <p>
-        This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
+        Secured using <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
       </p>
     </Layout>
   )

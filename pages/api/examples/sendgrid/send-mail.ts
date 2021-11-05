e9359edffe46d1462744ff5a5a7665f6c2fadb1f
Chandler Childs <rubberband75@gmail.com>
2021-11-05 07:10:15 +0000

    organize sendgrid api calls

---
+++ pages/api/examples/sendgrid/send-mail.ts
@@ -0,0 +1,23 @@
+import sgMail from "@sendgrid/mail"
+import { NextApiRequest, NextApiResponse } from "next"
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    //   const { email, subject, message, name } = req.body
+    const msg: sgMail.MailDataRequired = {
+      to: "Chandler Childs <rubberband75@gmail.com>",
+      from: process.env.EMAIL_FROM,
+      subject: "UpBids Test Email",
+      text: `*Anakin Voice*: It's working!....I'ts working!`,
+    }
+
+    try {
+      await sgMail.send(msg)
+      res.json({ message: `Email has been sent` })
+    } catch (error) {
+      res.status(500).json({ error: "Error sending email" })
+    }
+  } else res.status(405).json({ error: "Method Not Allowed" })
+}

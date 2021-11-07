ffde4021d7932e2430eeb63160f79d84bbdec942
Chandler Childs <rubberband75@gmail.com>
2021-11-07 07:14:18 +0000

    update account page frontend

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -3,69 +3,182 @@ import type { Session } from "next-auth"
 import { getSession } from "next-auth/react"
 import Layout from "../../components/layout"
 import useSwr from "swr"
-import React, { useState } from "react"
+import React, { useEffect, useState, useRef } from "react"
 import axios from "axios"
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json())
-
 export default function AccountIndex() {
-  let { data, mutate, error } = useSwr("/api/users/current", fetcher, {
-    revalidateOnFocus: false,
-  })
+  const imageInputRef = useRef() as React.MutableRefObject<HTMLInputElement>
 
-  const [state, setState] = useState({
+  let [errorMessage, setErrorMessage] = useState("")
+  let [loading, setLoading] = useState(true)
+  let [user, setUser] = useState({
     name: "",
     email: "",
     phone: "",
     image: "",
   })
+  const [selectedFile, setSelectedFile] = useState<File | null>()
+  const [previewImage, setPreviewImage] = useState("")
+  const [dataModified, setDataModified] = useState(false)
 
+  const getUser = () => {
+    setErrorMessage("")
+    resetImage()
+    axios
+      .get("/api/users/current")
+      .then((response) => {
+        setUser({
+          ...user,
+          name: response.data.user.name || "",
+          email: response.data.user.email || "",
+          phone: response.data.user.phone || "",
+          image: response.data.user.image || "",
+        })
+        setDataModified(false)
+      })
+      .catch((error) => {
+        console.error(error)
+        setErrorMessage(`${error}`)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+  useEffect(() => getUser(), [])
+
+  const updateUser = () => {
+    setErrorMessage("")
+
+    const formData = new FormData()
+    formData.append("name", user.name)
+    formData.append("email", user.email)
+    formData.append("phone", user.phone)
+    formData.append("image", user.image)
+
+    if (selectedFile) {
+      formData.append("file", selectedFile)
+    }
+
+    axios
+      .patch("/api/users/current", formData)
+      .then((response) => {
+        console.log("User Updated")
+        setUser({
+          ...user,
+          name: response.data.user.name || "",
+          email: response.data.user.email || "",
+          phone: response.data.user.phone || "",
+          image: response.data.user.image || "",
+        })
+        setDataModified(false)
+      })
+      .catch((error) => {
+        console.error(error)
+        setErrorMessage(`${error}`)
+      })
+  }
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-
-    // console.log({ e.target.name })
-    // this.props.searchUsers(this.state.text)
-    // this.setState({ text: "" })
+    updateUser()
   }
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
-    setState({ ...state, [e.currentTarget.name]: e.currentTarget.value })
+    setUser({ ...user, [e.currentTarget.name]: e.currentTarget.value })
+    setDataModified(true)
   }
 
-  const handleMutate = async (
-    e: React.FormEvent<HTMLInputElement>
-  ): Promise<void> => {
-    let mutated = { ...data }
-    mutated.user[e.currentTarget.name] = e.currentTarget.value
-    console.log(mutated.user)
-    let m = await mutate(mutated, false)
-    console.log({ m })
+  const updateImage = (e: React.FormEvent<HTMLInputElement>): void => {
+    let fileList: FileList | null = (e.target as HTMLInputElement).files
+    if (fileList && fileList[0]) {
+      let file = fileList[0]
+      setSelectedFile(file)
+
+      let reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.onload = (r) => {
+        let loadedImage = r.target?.result || ""
+        setPreviewImage(`${loadedImage}`)
+      }
+    }
+    setDataModified(true)
+  }
+
+  const resetImage = () => {
+    setSelectedFile(null)
+    setPreviewImage("")
+  }
+
+  const deleteImage = () => {
+    resetImage()
+    setUser({
+      ...user,
+      image: "",
+    })
+    setDataModified(true)
   }
 
   return (
     <Layout>
       <h1>My Account</h1>
-      <hr />
-      {error && (
+      {errorMessage && (
         // Error Message
-        <>
-          <div>An Error Occured!</div>
-          <p>{error}</p>
-        </>
+        <p className={"error-message"}>{errorMessage}</p>
       )}
-      {!data && (
+      {loading && (
         // Loaing Message
         <>
           <p>Loading...</p>
         </>
       )}
-      {!error && data && (
+      {!loading && (
         // Account Data Form
         <>
           <form onSubmit={handleSubmit}>
             <fieldset>
               <legend>Account Details</legend>
+              <p style={{ width: "fit-content" }}>
+                <div
+                  style={{
+                    backgroundImage: `url(${previewImage || user.image})`,
+                  }}
+                  className={"profilePicture"}
+                ></div>
+                <br />
+                <input
+                  hidden
+                  type="file"
+                  accept="image/*"
+                  ref={imageInputRef}
+                  // value={selectedFile}
+                  onChange={updateImage}
+                />
+                <button
+                  type="button"
+                  style={{ padding: ".5em" }}
+                  onClick={() => imageInputRef.current.click()}
+                >
+                  Update Image
+                </button>
+                {user.image && (
+                  <button
+                    type="button"
+                    className={"text-button"}
+                    onClick={deleteImage}
+                  >
+                    Delete
+                  </button>
+                )}
+                {previewImage && (
+                  <button
+                    type="button"
+                    className={"text-button"}
+                    onClick={resetImage}
+                  >
+                    Reset
+                  </button>
+                )}
+              </p>
               <p>
                 <label htmlFor="name">
                   <span>Full Name</span>
@@ -75,8 +188,8 @@ export default function AccountIndex() {
                   type="text"
                   id="name"
                   name="name"
-                  value={data.user.name}
-                  onChange={handleMutate}
+                  value={user.name}
+                  onChange={handleChange}
                 />
               </p>
               <p>
@@ -88,7 +201,7 @@ export default function AccountIndex() {
                   type="email"
                   id="email"
                   name="email"
-                  value={state.email}
+                  value={user.email}
                   onChange={handleChange}
                 />
               </p>
@@ -101,13 +214,18 @@ export default function AccountIndex() {
                   type="tel"
                   id="phone"
                   name="phone"
-                  value={state.phone}
+                  value={user.phone}
                   onChange={handleChange}
                 />
               </p>
             </fieldset>
             <br />
             <button type="submit">Save</button>
+            {dataModified && (
+              <button type="button" className={"text-button"} onClick={getUser}>
+                Cancel
+              </button>
+            )}
           </form>
         </>
       )}

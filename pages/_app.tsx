ebe0ebb042aa88347e14545274ecb63753978a7e
Chandler Childs <chandler@advenna.com>
2021-11-17 20:40:29 -0700

    add mui support to _app and _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,37 @@ import { SessionProvider } from "next-auth/react"
 import type { AppProps } from "next/app"
 import "./styles.css"
 
-export default function App({
+import * as React from "react"
+import Head from "next/head"
+import { ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+import { CacheProvider, EmotionCache } from "@emotion/react"
+import theme from "../components/theme"
+import createEmotionCache from "../components/createEmotionCache"
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+export default function MyApp({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps) {
+}: MyAppProps) {
   return (
-    // `session` comes from `getServerSideProps` or `getInitialProps`.
-    // Avoids flickering/session loading on first load.
-    <SessionProvider session={session} refetchInterval={5 * 60}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <CacheProvider value={clientSideEmotionCache}>
+      <Head>
+        <title>UpBids</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <SessionProvider session={session} refetchInterval={5 * 60}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SessionProvider>
+    </CacheProvider>
   )
 }
